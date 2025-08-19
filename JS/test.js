1d78d76c8e8f00ef2fa7ed2dc72193084e9575b2
YassenIsmail12 <Yassen.mohamed200222@gmail.com>
2025-08-19 13:56:16 +0200

    JS Day 1

---
+++ JS/test.js
@@ -0,0 +1,121 @@
+// var x = 5;
+// var y = new Number(7);
+
+// var s = "String"
+// var st = new String("STRING")
+
+// var b = false
+// var bol = new Boolean(true)
+
+// console.log(typeof(x))
+// console.log(typeof(y))
+// console.log(typeof(s))
+// console.log(typeof(st))
+// console.log(typeof(b))
+// console.log(typeof(bol))
+
+
+
+// console.log(x);A
+// var x = 5;
+
+// var x = 5;
+// var y = new Number(5)
+// console.log(x==y) //False!
+
+// var x = 123.4567;	
+// console.log(x.toFixed(2));
+// console.log(x.toPrecision(4));
+
+// var a = parseInt("a 11");
+// if (isNaN(a)) {
+//     console.log("YES");
+// }
+
+// console.log(parseInt("123px"));
+// console.log(parseFloat("123.45px"));
+// console.log(Number("123px"));
+
+// var i = "I am " + 23;
+// console.log(i);
+
+// var n = 45;
+// var s = String(n);
+// console.log(s);
+
+// console.log(true + 5);
+// console.log(typeof(true + 5));
+// console.log("10" - 2);
+// console.log(typeof("10" - 2));
+// console.log(12 - "1a");
+// console.log(typeof(12 - "1a"));
+// console.log(5 / 0);
+// console.log(typeof(5 / 0));
+// console.log(5+undefined);
+// console.log(typeof(5+undefined));
+
+// var a = "15.5";
+// var b = +a;
+// console.log(b, typeof b);
+
+// var result = 20 > true < 5 == 1;
+// console.log(result);
+
+// function canbenum (s) {
+//     x = parseInt(s);
+//     if (isNaN(x)){
+//         return false;
+//     }
+//     else {
+//         return true;
+//     }
+// }
+
+// s = "123pox";
+// console.log(canbenum(s));
+
+// var x = 1;
+// while (x<=20) {
+//     console.log(x);
+//     x = x+1;
+// }
+
+// var sum = 0;
+// var number;
+// do {
+//   number = parseInt(prompt("Enter numbers"));
+//   if (number !== 0) {
+//     sum += number;
+//   }
+// } while (number !== 0);
+// console.log("The sum : " + sum);
+
+// for (var i = 1; i <= 7; i++) {
+//   var day;
+//   switch (i) {
+//     case 1:
+//       day = "Sunday";
+//       break;
+//     case 2:
+//       day = "Monday";
+//       break;
+//     case 3:
+//       day = "Tuesday";
+//       break;
+//     case 4:
+//       day = "Wednesday";
+//       break;
+//     case 5:
+//       day = "Thursday";
+//       break;
+//     case 6:
+//       day = "Friday";
+//       break;
+//     case 7:
+//       day = "Saturday";
+//       break;
+//     default:
+//       day = "Invalid day";
+//   }
+//   console.log(i + " is " + day);
+// }
